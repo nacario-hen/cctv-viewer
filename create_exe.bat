537457303595c23b0d6b239385c5144cff34f0e1
Henrico <nacario.henrico.main@gmail.com>
2026-01-12 17:46:26 +0800

    Add some improvements to pyinstaller script

--- create_exe.bat
+++ create_exe.bat
@@ -1,3 +1,5 @@
 @echo off
 echo Creating executable file for CCTV Viewer
 py -m PyInstaller main.py --add-data "resources/;resources"
+@REM For single file, use the command below. Otherwise, default command is above
+@REM py -m PyInstaller main.py -F --add-data "resources/;resources" 
